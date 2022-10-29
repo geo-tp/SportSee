a8d8c4ae3f3ff559a1aa0f97b6b8335fb0ddb946
geo <tp53.geo@gmail.com>
2022-10-30 00:34:57 +0200

    change headers struct

--- src/api/headers.js
+++ src/api/headers.js
@@ -1,4 +1,4 @@
 export const HEADERS = {
-  method: "GET",
-  headers: { Accept: "application/json", "Content-Type": "application/json" },
+  Accept: "application/json",
+  "Content-Type": "application/json",
 };
